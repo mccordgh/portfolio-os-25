168b4257396d45c42e22c2541594e8c357a6e9a2
Matthew McCord <mattm.eternal@gmail.com>
2025-04-22 15:15:52 -0700

    redirect to base path if not there

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import './App.css'
-import  Desktop from './components/Desktop';
+import "./App.css";
+import Desktop from "./components/Desktop";
 
 function App() {
+  const loc = window.location;
+  const { pathname } = loc;
+
+  if (pathname !== "/") {
+    const { protocol, host } = loc;
+
+    window.location.href = `${protocol}//${host}/`;
+  }
+
   return (
     <div className="App">
       <Desktop />
