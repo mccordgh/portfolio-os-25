ee3efcd7c38a96f85b0eb50075c93835281cc8e8
Matthew McCord <mattm.eternal@gmail.com>
2025-04-28 12:25:35 -0700

    comment out redirection logic in App.tsx that can cause a bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css";
 import Desktop from "./components/Desktop";
 
+/* TODO:
+  - Update Portfolio about!!!
+  - utilize isMobileFriendly property
+  - Fix Padding on top mobile view status bar
+  - Fix view now link on Portfolio about.
+*/
 function App() {
-  const loc = window.location;
-  const { pathname } = loc;
+  // const loc = window.location;
+  // const { pathname } = loc;
 
-  if (pathname !== "/") {
-    const { protocol, host } = loc;
+  // if (pathname !== "/") {
+  //   const { protocol, host } = loc;
 
-    window.location.href = `${protocol}//${host}/`;
-  }
+  //   window.location.href = `${protocol}//${host}/`;
+  // }
 
   return (
     <div className="App">
