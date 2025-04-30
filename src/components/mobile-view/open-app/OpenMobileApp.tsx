24ba87767bc8a65f95a9e6298c067b72926144d1
Matthew McCord <mattm.eternal@gmail.com>
2025-04-29 18:16:52 -0700

    fix bug on mobile where music link missing

--- src/components/mobile-view/open-app/OpenMobileApp.tsx
+++ src/components/mobile-view/open-app/OpenMobileApp.tsx
@@ -10,6 +10,37 @@ type OpenMobileAppProps = {
 function OpenMobileApp(props: OpenMobileAppProps) {
   const { setSelectedApp, mode } = useContext(AppsContext);
   const { app } = props;
+  const {
+    directory,
+    headerImage,
+    iconImage,
+    links,
+    isMobileFriendly,
+    name,
+    shortText,
+    description,
+  } = app;
+
+  const image = `resources/${directory}/${headerImage}`;
+  const iconImagePath = `resources/${directory}/${iconImage}`;
+  const appLinks =
+    links && links.length ? (
+      <ul>
+        {links?.map((link, key) => {
+          // The first link will direct user to go play the game in a browser.
+          // When the user is on mobile, any game with isMobileFriendly set to false will not be playable.
+          if (
+            key === 0 &&
+            mode === "mobile" &&
+            !isMobileFriendly &&
+            directory === "game_dev"
+          ) {
+            return (
+              <li key={key}>
+                <span>(To play this game, please use a Desktop device)</span>
+              </li>
+            );
+          }
 
   const image = `resources/${app.directory}/${app.headerImage}`;
   const iconImage = `resources/${app.directory}/${app.iconImage}`;
